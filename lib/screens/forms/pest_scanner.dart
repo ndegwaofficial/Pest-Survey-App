0fc3161f43bb20c51622181799a2d7fdb4fc3089
ndegwaofficial <ndegwaofficial@gmail.com>
2024-09-17 11:19:15 +0300

    Fixed Camera Initialization Error

--- lib/screens/forms/pest_scanner.dart
+++ lib/screens/forms/pest_scanner.dart
@@ -20,14 +20,20 @@ class _PestScannerState extends State<PestScanner> {
   }
 
   Future<void> _initializeCamera() async {
-    cameras = await availableCameras();
-    _controller = CameraController(cameras[0], ResolutionPreset.high);
-    _initializeControllerFuture = _controller.initialize();
+    try {
+      cameras = await availableCameras();
+      _controller = CameraController(cameras[0], ResolutionPreset.high);
+      _initializeControllerFuture = _controller.initialize();
+      setState(() {}); // Rebuild the widget after initializing the controller
+    } catch (e) {
+      print('Error initializing camera: $e');
+    }
   }
 
   Future<void> _takePicture() async {
     try {
       await _initializeControllerFuture; // Wait for the camera to initialize
+
       final image = await _controller.takePicture(); // Capture the image
 
       // Save image to temporary directory
@@ -52,20 +58,26 @@ class _PestScannerState extends State<PestScanner> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Scan Pest')),
+      appBar: AppBar(title: const Text('Scan Pest')),
+      // FutureBuilder will only display the camera preview only after camera is fully initialized
       body: FutureBuilder<void>(
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller); // Display the camera preview
+            // If the future is complete, display the camera preview
+            return CameraPreview(_controller);
+          } else if (snapshot.hasError) {
+            // If an error occurred, display an error message
+            return Center(child: Text('Error: ${snapshot.error}'));
           } else {
+            // Otherwise, display a loading spinner
             return const Center(child: CircularProgressIndicator());
           }
         },
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera),
-        onPressed: _takePicture, // Take picture when button is pressed
+        child: const Icon(Icons.camera),
+        onPressed: _takePicture, // Capture image when button is pressed
       ),
     );
   }
